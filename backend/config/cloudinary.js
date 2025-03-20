08d9f6fcaa8740d91690728eec54b4c8e01abe16
SUGAVANESHWARAN S <154986511+Sugavanesh17@users.noreply.github.com>
2025-03-20 19:19:25 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -3,8 +3,8 @@ import { v2 as cloudinary } from "cloudinary";
 const connectCloudinary = async () => {
   cloudinary.config({
     cloud_name: "dhqklgrqc",
-    api_key: "863363933837532",
-    api_secret: "q_s1442kG4eMnYoHQuZfomfbDdI",
+    api_key: "/* Api key*/",
+    api_secret: "/* Api secret*/",
   });
 };
 export default connectCloudinary;
